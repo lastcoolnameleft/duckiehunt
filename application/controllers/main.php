cac4b2875f9c36cd715fb1c543f9b9fd4040ddce
Tommy Falgout <tommy@lastcoolnameleft.com>
2017-07-22 12:00:06 -0500

    Fix Google maps

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -26,17 +26,22 @@ class Main extends CI_Controller {
         //  Get latest locations
         $this->load->model('duck_model', 'duck');
 
-			$location_list = $this->duck->getLatestLocations();
+        $location_list = $this->duck->getLatestLocations();
+        $data = array(
+            'focus_lat' => 23,
+            'focus_long' => 10,
+            'focus_zoom' => 2,
+            'location_list' => array()
+        );
 
-            $data['location_list'] = array();
-
-			foreach ($location_list as $location) {
-                $data['duck_location_pulldown'][$location['duck_location_id']] = $location['location'];
-                $data['location_list'][] = $this->duck->renderLocation( $location );
-			}
+        foreach ($location_list as $location) {
+            $data['duck_location_pulldown'][$location['duck_location_id']] = $location['location'];
+            $data['location_list'][] = $this->duck->renderLocation( $location );
+        }
 
 //            $this->load->view('test/yui');
-		$this->load->view('main', $data);
+		$this->load->view('main');
+		$this->load->view('duck/view/map', $data);
 //        $this->load->view('top_distance_table_yui', $params);
 		$this->load->view('footer');
 	}
--- application/views/duck/view/map.php
+++ application/views/duck/view/map.php
@@ -1,79 +1,38 @@
-<style type="text/css">
-#map a:hover {
-    color:black;
-}
+<div id='map'></div>
 
-#map div {
-    color: black;
-font: 100 0.9em "trebuchet ms", serif;
- 
-}
-
-td.text {
-	color:blue;
-}
+<style>
+ #map {
+   width: 100%;
+   height: 400px;
+   background-color: grey;
+   color: #000;
+ }
 </style>
 
-<script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.8&appid=<?php echo $this->config->item('y_app_id'); ?>"></script>
-<script type="text/javascript" language="JavaScript">
-var map;
-function redirectToDuck(obj)
-{
-	var duck_id = obj[obj.selectedIndex].value;
-	var URL = '/view/duck/' + duck_id;
-	window.location.href = URL;
-}
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQ9N94fMjCedY84yfdWjIw3uGIuOC8ymU&callback=initMap">
+</script>
 
-function redirectToDuckLocation(obj)
-{
-	var location_id = obj[obj.selectedIndex].value;
-	var URL = '/view/location/' + location_id;
-	window.location.href = URL;
-}
-
-//        YLog.initSize(new YSize(600,300));
-        function StartYMap() {
-                map = new YMap(document.getElementById('map'),YAHOO_MAP_REG);
-                var cPT = new YGeoPoint(<?php echo $this->config->item('start_lat'); ?>, <?php echo $this->config->item('start_lon'); ?>);
-                map.drawZoomAndCenter(cPT,<?php echo $this->config->item('start_zoom'); ?>);
-                map.addTypeControl();
-                map.addZoomLong();
-                map.addPanControl();
-
+<script>
 <?php
-		$point_array = array();
-
-		if ( !empty($location_list) ) {
-			foreach ( $location_list as $index => $location) {
-				$js_point_name = "cPT{$index}";
-
-				$marker_name = "marker{$index}";
-				echo "\t\tvar {$js_point_name} = new YGeoPoint( {$location['latitude']}, {$location['longitude']} );\n";
-				echo "\t\tvar {$marker_name} = new YMarker({$js_point_name}, null, '{$marker_name}');\n";
-				echo "\t\tvar new_image = new YImage();\n";
-//				echo "\t\tnew_image.src = '" . (empty($location['thumbnail_url']) ? '/duck-16x16.png' : $location['thumbnail_url']) . "'\n";
-				echo "\t\tnew_image.src = '/images/icons/duck-32x32.png'\n";
-				echo "\t\t{$marker_name}.changeImage(new_image);\n";
-
-				if ( !empty($location['textbox']) ) {
-					$textbox = preg_replace("/\n/", '<br/>', $location['textbox']);
-//					$textbox = preg_replace('@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.#]*(\?\S+)?)?)?)@', '<a href="$1">$1</a>', $textbox);
-					echo "\t\tYEvent.Capture({$marker_name}, EventsList.MouseClick, function(){ {$marker_name}.openSmartWindow('<div>{$textbox}</div>');  });\n";
-				}
-
-				echo "\t\tmap.addOverlay({$marker_name});\n\n\n";
-				$point_array[] = $js_point_name;
-			}
-                // args: array of pts, color, width, alpha
-			echo "\t\tvar poly1 = new YPolyline([" . implode(',', $point_array) . "],'blue',7,0.7);\n";
-			echo "\t\tmap.addOverlay(poly1);\n\n";
-			echo "\t\tvar zoomcenter = map.getBestZoomAndCenter([" . implode(',', $point_array) . "]);\n\n";
-            echo "\t\tzoomcenter.zoomLevel = (zoomcenter.zoomLevel >= 18) ? 17 : zoomcenter.zoomLevel\n";
-            echo "\t\tmap.drawZoomAndCenter(zoomcenter.YGeoPoint, zoomcenter.zoomLevel );\n";
-
-		}
+	echo "var focus_lat={$focus_lat};\n";
+	echo "var focus_long={$focus_long};\n";
+	echo "var focus_zoom={$focus_zoom};\n";
+	echo "var location_list=" . json_encode($location_list) . ";\n";
 ?>
-        }
-        window.onload = StartYMap;
-        </script>
+	function initMap() {
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: focus_zoom,
+			center: {lat: focus_lat, lng: focus_long}
+		});
+		location_list.forEach((location, index) => {
+			var position = { lat: Number(location.latitude), lng: Number(location.longitude) };
+			var marker = new google.maps.Marker({ position: position, map: map, icon: '/images/icons/duck-32x32.png' });
+			var duck_header = '<a href="/view/duck/' + location.duck_id + '">Duck #' + location.duck_id + '</a>';
+			var content = '<div id="content"><p>' + duck_header + '<br/>' + location.comments + '</p></div>';
+			var infoWindow = new google.maps.InfoWindow( { content: content });
+			marker.addListener('click', function() { infoWindow.open(map, marker); });
+		});
 
+	}
+</script>
