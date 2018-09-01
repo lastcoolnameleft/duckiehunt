7f6e669f04ac6f43c6ae07db138ce3a06742e0eb
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-09-01 18:08:31 -0500

    Finally got simple test working!

--- django/duck/tests.py
+++ django/duck/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory, TestCase
+
+from .views import detail, mark, index
+
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+#        self.user = User.objects.create_user(
+#            username='jacob', email='tommy@duckiehunt.com', password='top_secret')
+
+    def test_details(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/')
+
+        # Or you can simulate an anonymous user by setting request.user to
+        # an AnonymousUser instance.
+#        request.user = AnonymousUser()
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
