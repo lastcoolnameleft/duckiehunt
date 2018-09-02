9cab80e1369c3924ee71bb405aa848efdda6e552
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-09-02 14:46:25 -0500

    Add admin page

--- django/duck/admin.py
+++ django/duck/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Duck
+from .models import Duck, DuckLocation, DuckLocationLink, DuckLocationPhoto
 
 admin.site.register(Duck)
+admin.site.register(DuckLocation)
+admin.site.register(DuckLocationLink)
+admin.site.register(DuckLocationPhoto)
 
