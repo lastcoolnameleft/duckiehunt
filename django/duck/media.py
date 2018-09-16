b87e46fac7150e4ca14e1a14c2b5ea55fdd8b504
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-09-16 13:51:24 -0500

    refactor flickr_api

--- django/duck/media.py
+++ django/duck/media.py
@@ -5,16 +5,11 @@ from django.core.files.storage import FileSystemStorage
 
 def handle_uploaded_file(uploaded_file, duck_id, duck_name, comments):
     """ Upload duck location image to flickr """
-    api_key = settings.FLICKR_API_KEY
-    api_secret = settings.FLICKR_API_SECRET
-    flickr_auth_file = settings.FLICKR_AUTH_FILE #'/tmp/authfile'
     title = 'Duck #' + str(duck_id) + ' (' + duck_name + ')'
-    is_public = "0"
     tags = "duckiehunt"
 
     file_path = write_upload_to_file(uploaded_file, settings.UPLOAD_PATH)
-    photo_info = upload_to_flickr(api_key, api_secret, flickr_auth_file, file_path,
-                                  title, comments, is_public, tags)
+    photo_info = upload_to_flickr(file_path, title, comments, settings.FLICKR_PHOTO_IS_PUBLIC, tags)
     return photo_info
 
 def write_upload_to_file(photo_file, upload_path):
@@ -24,14 +19,10 @@ def write_upload_to_file(photo_file, upload_path):
     uploaded_file_url = fss.url(filename)
     return uploaded_file_url
 
-def upload_to_flickr(api_key, api_secret, flickr_auth_file, photo_file,
-                     title, comments, is_public, tags):
+def upload_to_flickr(photo_file, title, comments, is_public, tags):
     """ Upload file to flickr """
-    flickr_api.set_keys(api_key=api_key, api_secret=api_secret)
-    flickr_api.set_auth_handler(flickr_auth_file)
     photo = flickr_api.upload(photo_file=photo_file, title=title, is_public=is_public,
                               tags=tags, description=comments)
     photo_info = photo.getInfo()
     photo_info['sizes'] = photo.getSizes()
-    print(photo_info)
     return photo_info
